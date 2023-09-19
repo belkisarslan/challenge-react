9b29feec27d7335c0b1798b8965e6fd4ad75eb76
belkisarslan <belkisarslan33@gmail.com>
2023-09-19 12:18:28 +0300

    Different components at the same position reset state

--- src/components/sharing.js
+++ src/components/sharing.js
@@ -6,6 +6,7 @@ export default function Sharing() {
    
     const [query, setQuery] = useState('');
     const results = filterItems(foods, query);
+    const [isPaused, setIsPaused] = useState(false)
   
     function handleChange(e) {
       setQuery(e.target.value);
@@ -17,10 +18,24 @@ export default function Sharing() {
     <h4 className='p-3 pl-8 text-center text-gray-500 text-xl font-semibold leading-8'>Challenge 2 of 2: Filtering a list</h4>
         <p className='text-center'><a target='_blank' className='p-3 pl-8 text-cyan-900 text-l font-semibold leading-4' href='https://react.dev/learn/sharing-state-between-components'>Click for challenge page</a></p>
     <div className='h-80 overflow-y-auto p-5 my-2 bg-gradient-to-r from-gray-800 flex flex-col items-center relative'>
-    <SearchBar
+    {isPaused ? (
+        <p className='font-medium text-teal-500 m-1'>See you later!</p> 
+      ) : (
+        <SearchBar
         query={query}
         onChange={handleChange}
       />
+      )}
+       <label className='font-medium text-cyan-800 m-2'>
+        <input className='w-4 h-4 mr-2 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500'
+          type="checkbox"
+          checked={isPaused}
+          onChange={e => {
+            setIsPaused(e.target.checked)
+          }}
+        />
+        Are you tired? let's take a break
+      </label>
       <hr />
       {results.length ? <List items={results} /> : <p className='text-red-300 mt-10'>~ Not Founded ~</p>}
     </div>
